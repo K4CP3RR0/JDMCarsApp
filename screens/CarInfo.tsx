42d3494ac27dc815baef1075c7bf10ed8458f156
K4CP3RR0 <kacek.cichy@gmail.com>
2022-01-28 20:13:48 +0100

    j

--- screens/CarInfo.tsx
+++ screens/CarInfo.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '../components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Car Info</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
   },
   title: {
     fontSize: 20,
